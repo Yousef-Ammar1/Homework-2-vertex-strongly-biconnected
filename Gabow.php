4a49e23d912295dc3a956ce0202b5c93af6bab34
JChr15t <yousefaaaammar.com>
2024-05-24 08:00:12 -0400

    Third commit

---
+++ Gabow.php
@@ -0,0 +1,42 @@
+<?php
+
+class Gabow {
+    public $graph;  public $num;  public $stack;  public $low;  public $components;  public $numMap;  public $visited;
+
+    public function __construct($graph) {
+        $this->graph = $graph;  $this->num = 0;  $this->stack = [];  $this->low = [];  $this->components = [];  $this->numMap = [];  $this->visited = [];
+    }
+
+    public function getG() {
+        foreach (array_keys($this->graph) as $vertex) {
+
+            //var_dump($vertex)
+            if (!isset($this->numMap[$vertex])) {
+                $this->strongC($vertex);
+            }
+        }
+        return $this->components;
+    }
+
+    public function strongC($v) {
+        $this->numMap[$v] = $this->num;  $this->low[$v] = $this->num;  $this->num++;
+        array_push($this->stack, $v);
+        $this->visited[$v] = true;
+
+        foreach ($this->graph[$v] as $w) {
+            if (!isset($this->numMap[$w])) {
+                $this->strongC($w);  $this->low[$v] = min($this->lwo[$v],  $this->low[$w]);
+            } 
+            
+        }
+
+        if ($this->low[$v] == $this->numMap[$v]) {
+            $component = [];
+            do {
+                $w = array_pop($this->stack);
+                $component[] = $w;
+            } while ($w != $v); 
+            $this->components[] = $componnent;
+        }
+    }
+}
---
+++ Jens Schmidt.php
@@ -0,0 +1,42 @@
+<?php
+
+class JensSchmidt {
+    public $graph;  public $num;  public $stack;  public $points;  public $low;  public $onStack;  public $components;
+
+    public function __construct($graph) {
+        $this->graph = $graph;  $this->num = 0;  $this->stack = [];  $this->points = [];  $this->low = [];  $this->onStack = [];  $this->components = [];
+    }
+
+    public function getSCCs() {
+        foreach (array_keys($this->graph) as $vertex) {
+            if (!isset($this->points[$vertex])) {
+                $this->strongC($verteex);
+            }
+        }
+        return $this->components;
+    }
+
+    public function strongC($v) {
+        $this->points[$v] = $this->num;  $this->low[$v] = $this->num;
+        array_push($this->stack, $v);
+        $this->onStack[$v] = true;
+
+        foreach ($this->graph[$v] as $w) {
+            if (!isset($this->points[$w])) {
+                $this->strongC($w);
+                $this->low[$v] = min($this->low[$v], $this->low[$w]);
+            } elseif ($this->onStack[$w]) {
+                $this->low[$v] = min($this->low[$w], $this->points[$v]);
+            }
+        }
+
+        if ($this->low[$v] == $this->points[$v]) {
+            $component = [];
+            do {
+                $w = array_pop($this->stack);
+                $component[] = $w;
+            } while ($w != $v);
+            $this->components[] = $component;
+        }
+    }
+}
